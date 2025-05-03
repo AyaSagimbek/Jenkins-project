4b0c4369bcf53d675d440966aa8eb32a7c5425db
AyaSagimbek <sagimbekayaulym@gmail.com>
2025-05-03 04:50:21 +0000

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,26 @@
-resource "null_resource" "waiting" {
-  depends_on = [aws_instance.web]
+module "project" {
+  source  = "AyaSagimbek/project/jenkins"
+  version = "2.0.0"
+  # insert the 7 required variables here
+  region = "us-east-1"
+vpc_cidr = "10.0.0.0/16"
+subnet1_cidr = "10.0.1.0/24"
+subnet2_cidr = "10.0.2.0/24"
+subnet3_cidr = "10.0.3.0/24"
+ip_on_launch = true
+instance_type = "t2.micro"
 
-  provisioner "local-exec" {
-    command = "echo 'Waiting for EC2 to boot...' && sleep 60"
-  }
 }
 
-resource "null_resource" "ansible_provisioner" {
-  depends_on = [null_resource.waiting]
+output "aws_instance_public_ip"  {
+  value = module.project.ec2
+}
 
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${aws_instance.web.public_ip}, ../ansible/main.yml  --private-key ~/.ssh/id_rsa"
+terraform {
+  backend "s3" {
+    bucket = "kaizen-group1-project-jenkins"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
   }
 }
